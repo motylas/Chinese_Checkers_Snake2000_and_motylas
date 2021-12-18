2fdc14cf11cddc1718c9f704faf4f04f10974722
S2n0a0k0e <88397167+S2n0a0k0e@users.noreply.github.com>
2021-12-18 23:22:05 +0100

    Added Window class, created Piece class and added constructor to Tile class. Window displays board and ActionListeners allows to move clicked tiles.

---
+++ src/main/java/Piece.java
@@ -0,0 +1,8 @@
+public class Piece {
+  int x;
+  int y;
+  public Piece(int x, int y){
+    this.x = x;
+    this.y = y;
+  }
+}
--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -1,4 +1,10 @@
 public class Tile {
   int x;
   int y;
+  boolean isTaken;
+  public Tile(int x, int y, boolean  isTaken){
+    this.x = x;
+    this.y = y;
+    this.isTaken = isTaken;
+  }
 }
