0e8e34b3c211c2cad0d7d50038c911732eba4dac
S2n0a0k0e <88397167+S2n0a0k0e@users.noreply.github.com>
2021-12-20 20:53:43 +0100

    Modified constructor Tile (added variables 'isAvailable', 'R', 'G', 'B')

--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -2,9 +2,17 @@ public class Tile {
   int x;
   int y;
   boolean isTaken;
-  public Tile(int x, int y, boolean  isTaken){
+  boolean isAvailable;
+  int R;
+  int G;
+  int B;
+  public Tile(int x, int y, boolean  isTaken, boolean isAvailable, int R, int G, int B){
     this.x = x;
     this.y = y;
     this.isTaken = isTaken;
+    this.isAvailable = isAvailable;
+    this.R = R;
+    this.G = G;
+    this.B = B;
   }
 }
