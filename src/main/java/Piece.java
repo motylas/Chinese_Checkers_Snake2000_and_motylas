799cc49eed830d713e4590c91a0de16bc232cc96
Hubert <hgruda68@gmail.com>
2021-12-21 01:50:15 +0100

    Added ID and ID getter

--- src/main/java/Piece.java
+++ src/main/java/Piece.java
@@ -1,16 +1,22 @@
 public class Piece {
+  int id;
   int x;
   int y;
   boolean isActive;
   int R;
   int G;
   int B;
-  public Piece(int x, int y, boolean isActive,int R, int G, int B){
+  public Piece(int id, int x, int y, boolean isActive,int R, int G, int B){
+    this.id=id;
     this.x = x;
     this.y = y;
     this.isActive = isActive;
     this.R = R;
     this.G = G;
     this.B = B;
+  }
+
+  public int getID(){
+    return id;
   }
 }
