0b9f5f87e7bba4fc5a78b8f1ee69edc555ca0810
Hubert <hgruda68@gmail.com>
2022-01-06 00:49:20 +0100

    added maxPlayers and playerCount now u can disconnect from game and join during game

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,19 +2,40 @@ import java.net.ServerSocket;
 import java.util.concurrent.Executors;
 
 public class Server {
+    private static int playerCount = 1;
+    private static int maxPlayers=2;
+
     public static void main(String[] args) throws Exception{
         try (var listener = new ServerSocket(55555)){
             System.out.println("Chinese checkers server is running...");
             var pool = Executors.newFixedThreadPool(20);
             while(true){
                 GameManager gm = new GameManager();
-                pool.execute(new Player(listener.accept(),gm));
-                pool.execute(new Player(listener.accept(),gm));
-                pool.execute(new Player(listener.accept(),gm));
-                pool.execute(new Player(listener.accept(),gm));
-                pool.execute(new Player(listener.accept(),gm));
-                pool.execute(new Player(listener.accept(),gm));
+                while(true){
+                    if(playerCount <= maxPlayers){
+                        System.out.println("siema");
+                        pool.execute(new Player(listener.accept(),gm, playerCount));
+                        playerCount++;
+                        System.out.println(playerCount);
+                    }
+                    System.out.print("");
+                }
             }
         }
+    }
+
+    static int getPlayerCount(){
+        return playerCount;
+    }
+    static int getMaxPlayers(){
+        return maxPlayers;
+    }
+
+    static void playerLeft(){
+        playerCount--;
+        System.out.println(playerCount);
+    }
+    static void setMaxPlayers(int maxPlayers){
+        Server.maxPlayers = maxPlayers;
     }
 }
