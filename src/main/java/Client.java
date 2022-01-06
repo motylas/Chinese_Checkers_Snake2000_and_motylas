8c8e3e6764cd5bec3cda5cd22e65a8f39b73fb88
Hubert <hgruda68@gmail.com>
2022-01-06 13:02:51 +0100

    added full working Lobby Players can join and when host starts the game all players get into the game

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -9,7 +9,8 @@ public  class Client {
     private Socket socket;
     private Scanner in;
     private static PrintWriter out;
-    Window var1;
+    static Window var1;
+    Lobby lobby;
 
     public Client(String serverAddress) throws Exception {
         try{
@@ -35,13 +36,24 @@ public  class Client {
             }
             else if (response.startsWith("START")){
                 var1.setVisible(true);
+                lobby.setVisible(false);
             }
             else if (response.startsWith("LOBBY")){
-
+                lobby = new Lobby();
+                out.println("JOIN");
             }
-            System.out.println("halo123");
+            else if (response.startsWith("HOST")){
+                lobby = new HostLobby();
+                out.println("JOIN");
+            }
+            else if (response.startsWith("JOIN")){
+                String[] values = response.split(";");
+                int playerCount = Integer.parseInt(values[1]);
+                lobby.playerJoin(playerCount);
+            }
             // TODO: 12/18/2021 tu sie przesyla od playera info jakies i bedzie wysylane do window zeby cos konkretnego wyswietlic
         }
+        exit(-1);
     }
 
     public static void action(String action){
@@ -50,6 +62,9 @@ public  class Client {
         }
         else if (action.startsWith("STOP")){
             out.println("STOP");
+        }
+        else if (action.startsWith("START")){
+            out.println("START");
         }
     }
 
